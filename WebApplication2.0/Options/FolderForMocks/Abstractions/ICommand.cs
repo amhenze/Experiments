5010afdb362632e2194e245ee1ec83b28d33af2c
Ракитин Илья Андреевич <ilyageqmer@mail.ru>
2023-01-29 22:37:31 +0300

    add sql and docker scripts

---
+++ WebApplication2.0/Options/FolderForMocks/Abstractions/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using Npgsql;
+
+namespace WebApplication2._0.Options.FolderForMocks.Abstractions
+{
+	public interface ICommand
+	{
+		ICollection<NpgsqlParameter> Parameters { get; }
+		public int ExecuteNonQuery();
+		public NpgsqlDataReader ExecuteReader();
+		Task<ISQLReader> ExecuteReaderAsync();
+	}
+}
